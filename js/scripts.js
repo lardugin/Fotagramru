ea6056e58dd2b2e8e8256b47bc93aa3fb79c9d5d
Ilnar Nizamutdinov <in@multiship.ru>
2014-12-20 18:01:23 +0300

    menu open/close js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 
+  var menu = $('.menu');
+
+  $('.toggle-nav').on( 'click', function(){
+
+
+    console.log('toggle-nav click');
+
+    if ( !menu.hasClass('active-mobile-menu') ) {
+
+      menu.addClass('active-mobile-menu');
+
+      $('.curtain').addClass('active');
+
+    } else {
+
+      menu.removeClass('active-mobile-menu');
+
+      $('.curtain').removeClass('active');
+
+    }
+
+  });
+
+
+
 });
