1172b5d6b257f2bb197331adecf3b6a5e5bbdb9a
Ilnar Nizamutdinov <in@multiship.ru>
2014-12-21 14:52:15 +0300

    dropdown selected item

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,60 @@ $(document).ready(function(){
 
   });
 
+  // upload-dropdown
+
+  $('.upload-dropdown-holder').on( 'click', function(){
+
+    if ( !$(this).hasClass('active') ) {
+
+      $('.upload-dropdown-menu').addClass('active');
+      $(this).addClass('active');
+      $('.upload-place__content').addClass('overlay');
+
+
+
+    } else {
+
+      $('.upload-dropdown-menu').removeClass('active');
+      $(this).removeClass('active');
+      $('.upload-place__content').removeClass('overlay');
+
+    }
+
+  });
+
+
+  $('#selectInstagram').on('click', function(){
+
+    $('.upload-dropdown-holder').hide().removeClass('active');
+    $('.upload-dropdown-menu').removeClass('active');
+    $('.upload-dropdown-holder.type_instagram').show();
+    $('.upload-place__content').removeClass('overlay');
+
+    $('.upload-dropdown-menu__item').removeClass('selected');
+    $(this).addClass('selected');
+
+    $('.upload-place-select').hide();
+    $('.upload-place-select.type_instagram').show();
+
+  });
+
+  $('#selectComputer').on('click', function(){
+
+    $('.upload-dropdown-holder').hide().removeClass('active');
+    $('.upload-dropdown-menu').removeClass('active');
+    $('.upload-dropdown-holder.type_computer').show();
+    $('.upload-place__content').removeClass('overlay');
+
+    $('.upload-dropdown-menu__item').removeClass('selected');
+    $(this).addClass('selected');
+
+    $('.upload-place-select').hide();
+    $('.upload-place-select.type_computer').show();
+
+  });
+
+
   $('.submenu-trigger').hover(function(){
     $('.curtain').addClass('curtain-active');
   }, function() {
